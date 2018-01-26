46f828d13e88e7e50bbb6f36a48e0a13cd9ce912
Robby <ta7382@gmail.com>
2018-01-27 02:01:21 +0800

    change page title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: '聊天室範例 | Tocas UI'
+    title: 'ChatRoom-With-SocketIO'
   });
 });
 
